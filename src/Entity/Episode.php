c6b52d9b01be70cf5ec6d9c35f8d25aacec8a65d
Kayriss951 <83235557+Kayriss951@users.noreply.github.com>
2021-11-30 10:22:01 +0100

    projetNaruto

---
+++ src/Entity/Episode.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EpisodeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EpisodeRepository::class)
+ */
+class Episode
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $pathToVideo;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPathToVideo(): ?string
+    {
+        return $this->pathToVideo;
+    }
+
+    public function setPathToVideo(string $pathToVideo): self
+    {
+        $this->pathToVideo = $pathToVideo;
+
+        return $this;
+    }
+}
